2b3432571c9e66540fb23826ab719af4ed535037
Koichiro Sumi <koichiro.sumi@actcat.co.jp>
2014-02-18 23:29:22 +0900

    debugging...

--- docker-rspec.rb
+++ docker-rspec.rb
@@ -75,7 +75,28 @@ begin
 
         ch.on_close { puts "done!" }
       end
+
+      ch.exec "#{ssh.exec! 'echo $PATH'}" do |ch, success|
+        raise "could not execute command" unless success
+
+        # "on_data" is called when the process writes something to stdout
+        ch.on_data do |c, data|
+          $stdout.print data
+          puts data
+        end
+
+        # "on_extended_data" is called when the process writes something to stderr
+        ch.on_extended_data do |c, type, data|
+          $stderr.print data
+          puts data
+        end
+
+        ch.on_close { puts "done!" }
+      end
+
     end
+
+    channel.wait
 
     puts "PATH = #{ssh.exec! 'echo $PATH'}"
   end
