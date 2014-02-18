fdfee9f66bc31308cb863d721b85286f48bf1602
Koichiro Sumi <koichiro.sumi@actcat.co.jp>
2014-02-18 23:49:18 +0900

    debugging...

--- docker-rspec.rb
+++ docker-rspec.rb
@@ -58,7 +58,7 @@ begin
     puts "PATH = #{ssh.exec! 'ruby -v'}"
 
     channel = ssh.open_channel do |ch|
-      ch.exec "source /usr/local/rvm/scripts/rvm; echo $PATH; echo 'path changing...';" do |ch, success|
+      ch.exec "source /usr/local/rvm/scripts/rvm; echo 'path changing...';" do |ch, success|
         raise "could not execute command" unless success
 
         # "on_data" is called when the process writes something to stdout
@@ -78,7 +78,7 @@ begin
 
       channel.wait
 
-      ch.exec "echo $PATH; echo 'path changed'" do |ch, success|
+      ch.exec "echo $PATH; echo 'path changed';" do |ch, success|
         raise "could not execute command" unless success
 
         # "on_data" is called when the process writes something to stdout
@@ -96,6 +96,7 @@ begin
         ch.on_close { puts "done!" }
       end
 
+      channel.wait
     end
 
     channel.wait
